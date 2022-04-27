c5d2b2179cb99220e2104dc232eb10994b4702c4
Joseph Senecal <jasenecal2@gmail.com>
2022-04-27 17:20:12 -0600

    Ref: total_discount to active_record only

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -25,14 +25,25 @@ class Invoice < ApplicationRecord
   end
 
   def discounted_rev
-    sum = 0.0
-    all_discounts = BulkDiscount.all.order(:discount)
-    invoice_items.each do |invoice_item|
-      item_discount = 0.0
-      all_discounts.each { |merchant_discount|item_discount = merchant_discount.discount if merchant_discount.threshold <= invoice_item.quantity}
-      discounted_item_total = (1-(item_discount/100)) * invoice_item.quantity * invoice_item.unit_price
-      sum += discounted_item_total
-    end
-    return sum
+    total_rev - total_discount
+  end
+
+  def total_discount
+    # sum = 0.0
+    # all_discounts = BulkDiscount.all.order(:discount)
+    # invoice_items.each do |invoice_item|
+    #   item_discount = 0.0
+    #   all_discounts.each { |merchant_discount|item_discount = merchant_discount.discount if merchant_discount.threshold <= invoice_item.quantity}
+    #   discounted_item_total = (1-(item_discount/100)) * invoice_item.quantity * invoice_item.unit_price
+    #   sum += discounted_item_total
+    # end
+    # return sum
+    invoice_items
+     .joins(:bulk_discounts)
+     .where('invoice_items.quantity >= bulk_discounts.threshold')
+     .select('invoice_items.id, invoice_items.quantity, invoice_items.unit_price, max(((bulk_discounts.discount/100))*invoice_items.unit_price*invoice_items.quantity) AS dis_rev')
+     .group('invoice_items.id')
+     .sum(&:dis_rev)
+     .to_f
   end
 end
